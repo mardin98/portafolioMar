c04218e0e4e1a92a8db5b97fb2392a85d1c7c037
mardin98 <44739224+mardin98@users.noreply.github.com>
2022-05-15 07:54:02 -0600

    Mardin Update

--- src/component/Hero/Home.js
+++ src/component/Hero/Home.js
@@ -1,10 +1,10 @@
 import React from "react"
 import "./Home.css"
 import hero from "../pic/hero.png"
-import react1 from "../pic/react.png"
-import skill2 from "../pic/Mern.png"
-import skill3 from "../pic/jst.png"
 import { Typewriter } from "react-simple-typewriter"
+
+
+
 
 
 const Home = () => {
@@ -20,25 +20,19 @@ const Home = () => {
             <h2>
               a
               <span>
-                <Typewriter words={[" Professional Coder.", " Developer."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
+                <Typewriter words={[" Professional Coder.", " Developer."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} autoStart={true} />
               </span>
             </h2>
 
             <p>I use animation as a third dimension by which to simplify experiences and kuiding thro each and every interaction. I’m not adding motion just to spruce things up, but doing it in ways that.</p>
 
             <div className='hero_btn d_flex'>
-              
               <div className='col_1'>
                 <h4>BEST SKILL ON</h4>
-                <button className='btn_shadow'>
-                  <img src={react1} alt='' />
-                </button>
-                <button className='btn_shadow'>
-                  <img src={skill2} alt='' />
-                </button>
-                <button className='btn_shadow'>
-                  <img src={skill3} alt='' />
-                </button>
+               
+                <img className="icons" src="https://img.icons8.com/nolan/96/react-native.png"></img>
+                <img className="icons icons1" src="https://img.icons8.com/nolan/96/javascript.png"/>
+                <img className="icons icons2" src="https://img.icons8.com/nolan/96/html.png"/>
               </div>
             </div>
           </div>
